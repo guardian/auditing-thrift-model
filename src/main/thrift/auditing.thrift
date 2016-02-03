e98476f284db7522809708446c74072ded8b3e2a
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-02-03 13:53:17 +0000

    include documentation and make some fields optional

--- src/main/thrift/auditing.thrift
+++ src/main/thrift/auditing.thrift
@@ -1,19 +1,29 @@
 namespace scala com.gu.auditing.model.v1
 
+/** List of applications **/
 enum App {
     FaciaTool = 1
+    StoryPackages = 2
 }
 
+/** Basic message **/
 struct Notification {
-    1: required string resourceId;
+    /** Application sending the message **/
+    1: required App app;
 
+    /** action / operation happening in the tool **/
     2: required string operation;
 
-    3: required string user;
+    /** email of the person making the operation **/
+    3: required string userEmail;
 
-    4: required App app;
+    /** ISO 8601 date of when the operation occurred **/
+    4: required string date;
 
-    5: required string message;
+    /** Identifies a resource in the source application **/
+    5: optional string resourceId;
 
-    6: required string date;
+    /** Additional data relevant to the source application **/
+    6: optional string message;
+
 }
